5f35731da7171ced47722f53cd08ae46e5dbde04
Melanie Downing <sayanything830@gmail.com>
2018-01-29 09:57:11 -0800

    fixed readme and index

--- lab-melanie/index.js
+++ lab-melanie/index.js
@@ -5,13 +5,13 @@ this.stack = new Stack();
 this.queue = new Queue();
 
 console.log('This is the stack module example:');
-console.log('adding first node ', this.stack.push(parseInt(process.argv[2])));
-console.log('adding second node ', this.stack.push(parseInt(process.argv[3])));
-console.log('adding thrid node ', this.stack.push(parseInt(process.argv[4])));
-console.log('current stack size ', this.stack.size);
+console.log('adding first node: ', this.stack.push(parseInt(process.argv[2])));
+console.log('adding second node: ', this.stack.push(parseInt(process.argv[3])));
+console.log('adding third node: ', this.stack.push(parseInt(process.argv[4])));
+console.log('current stack size: ', this.stack.size);
 console.log('top node value: ', this.stack.top.value);
-console.log('removing top node ', this.stack.pop());
-console.log('peek at new top node ', this.stack.peek());
+console.log('removing top node: ', this.stack.pop());
+console.log('peek at new top node: ', this.stack.peek());
 console.log('new stack size: ', this.stack.size);
 console.log('top node value: ', this.stack.top.value);
 
